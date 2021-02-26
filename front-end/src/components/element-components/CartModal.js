f9257d126f47bf27d8e0dc63cb7d12624e0dd59b
Fiip M <filipmitrovic@hotmail.com>
2021-02-26 17:52:56 +0100

    gjort klart varukorg funktionalitet

--- front-end/src/components/element-components/CartModal.js
+++ front-end/src/components/element-components/CartModal.js
@@ -1,33 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import CartIcon from '../element-components/CartIcon';
+// redux
+import { useDispatch } from 'react-redux';
+import increaseProductQuantity from '../../redux/actions/increaseProductQuantity';
+import decreaseProductQuantity from '../../redux/actions/decreaseProductQuantity';
 
-export default function CartModal({ closeModal }) {
+export default function CartModal() {
 
     const [localData, setLocalData] = useState([]);
-    
+    const dispatch = useDispatch();
+
     useEffect(() => {
         setLocalData(JSON.parse(localStorage.getItem('myCart')));
     }, [])
+    
+    const handleIncreaseQuantity = (id) => {
+        // loopa igenom localData och increaseProductQuantity på den och gör samma på den i localstorage
+        for (const obj of localData) {
+            if (obj.id === id) {
+                obj.quantity++;
+                dispatch(increaseProductQuantity(id));
+            }
+        }
+        localStorage.setItem('myCart', JSON.stringify(localData));
+        setLocalData(JSON.parse(localStorage.getItem('myCart')));    
+    }
+
+    const handleDecreaseQuantity = (id) => {
+        for (const obj of localData) {
+            if (obj.id === id) {
+                obj.quantity--;
+                dispatch(decreaseProductQuantity(id));
+            }
+        }
+        localStorage.setItem('myCart', JSON.stringify(localData));
+        setLocalData(JSON.parse(localStorage.getItem('myCart')));
+    }
 
     return (
         <div>
             <h1>Din beställning</h1>
-            <i onClick={() => closeModal()}>X</i>
             <div>
-                {localData.map((data) => (
-                    <div key={data.id}>
-                        <div>
-                            <h3>{data.title}</h3>
-                            <p>{data.price}</p>
-                        </div>
-                        <div>
-                            <hr/>
-                        </div>
-                        <div>
-                            antal
-                        </div>
+                {((localData === null) || (localData === undefined)) ? null : 
+                    <div>
+                        {localData.map((data) => (
+                            <div key={data.id} style={productItem}>
+                                <div>
+                                    <h3>{data.title}</h3>
+                                    <p>{data.price}</p>
+                                </div>
+                                <div>
+                                    <hr/>
+                                </div>
+                                <div>
+                                    <i onClick={() => handleIncreaseQuantity(data.id)}>↑</i>
+                                    <p>{data.quantity}</p>
+                                    <i onClick={() => handleDecreaseQuantity(data.id)}>↓</i>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                } 
                 <div>
                     <h3>Total</h3>
                     <p>200KR</p>
@@ -37,4 +69,10 @@ export default function CartModal({ closeModal }) {
             </div>
         </div>
     )
+}
+
+const productItem = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '20px'
 }
